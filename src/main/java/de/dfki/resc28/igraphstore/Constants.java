7875601d51fb2ffa510bb49a79e21a99f27512d8
René Schubotz <rene.schubotz@dfki.de>
2016-09-29 10:14:28 +0200

    more content type definitions

--- src/main/java/de/dfki/resc28/igraphstore/Constants.java
+++ src/main/java/de/dfki/resc28/igraphstore/Constants.java
@@ -11,21 +11,29 @@ import javax.ws.rs.core.MediaType;
  */
 public class Constants 
 {
-	// Content types
+	// RDF Content types
 	public static final String CT_APPLICATION_JSON = MediaType.APPLICATION_JSON;
-	public static final String CT_APPLICATION_LD_JSON = "application/ld+json";
-	public static final String CT_APPLICATION_RDFXML = "application/rdf+xml";
+	public static final String CT_APPLICATION_JSON_LD = "application/ld+json";
 	public static final String CT_APPLICATION_XTURTLE = "application/x-turtle";
+	public static final String CT_APPLICATION_NQUADS = "application/n-quads";
+	public static final String CT_APPLICATION_NTRIPLES = "application/n-triples";
+	public static final String CT_APPLICATION_RDF_JSON = "application/rdf+json";
+	public static final String CT_APPLICATION_RDFXML = "application/rdf+xml";
+	public static final String CT_APPLICATION_TRIX = "application/trix";
+	public static final String CT_TEXT_N3 = "text/n3; charset=utf-8";
+	public static final String CT_TEXT_TRIG = "text/trig";
+	public static final String CT_TEXT_TURTLE = "text/turtle";
+		
+	// SPARQL Content types
 	public static final String CT_APPLICATION_SPARQLQUERY = "application/sparql-query";
-	
 	public static final String CT_APPLICATION_SPARQLRESULTS_BIO = "application/sparql-results+bio";
 	public static final String CT_APPLICATION_SPARQLRESULTS_CSV = "application/sparql-results+csv";
 	public static final String CT_APPLICATION_SPARQLRESULTS_JSON = "application/sparql-results+json";
 	public static final String CT_APPLICATION_SPARQLRESULTS_SSE = "application/sparql-results+sse";
 	public static final String CT_APPLICATION_SPARQLRESULTS_TSV = "application/sparql-results+tsv";
 	public static final String CT_APPLICATION_SPARQLRESULTS_XML = "application/sparql-results+xml";
-	
+
+	// HTML & plain text content types
 	public static final String CT_TEXT_HTML = MediaType.TEXT_HTML;
-	public static final String CT_TEXT_TURTLE = "text/turtle";
-	public static final String CT_TEXT_TRIG = "text/trig";
+	public static final String CT_TEXT_PLAIN = "text/plain";
 }
